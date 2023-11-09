24c5dbd999043b7db4137c71c3775a54130bb5bd
Sergio Brü <89431263+zzergAtStage@users.noreply.github.com>
2023-11-09 05:03:31 +0100

    seminar03 homework

---
+++ src/main/java/seminars/third/hw/UserRepository.java
@@ -0,0 +1,28 @@
+package seminars.third.hw;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+
+    // Тут можно хранить аутентифицированных пользователей
+    List<User> data = new ArrayList<>();
+
+    public void addUser(User user) {
+       data.add(user);
+    }
+
+    public boolean findByName(String username) {
+        for (User user : data) {
+            if (user.name.equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void logOffNonAdminUsers(){
+        data.removeIf(user -> !user.isAdmin);
+    }
+
+}
